73a47a1b7e69f18e9591bdc46d802ad1e0361d3e
Maxiltron <147034131+Maxiltron@users.noreply.github.com>
2023-11-24 11:46:21 +0100

    Add files via upload

--- Plus_ou_Moins.py
+++ Plus_ou_Moins.py
@@ -2,14 +2,16 @@ import random
 
 
 def response_replay(message:str):
+    value: str = input(message)
+    value_2: str = "yes"
+    value_3: str = "no"
     while True:
-        value:str = input(message)
-        if value == "yes":
+        if value == value_2:
             return value
-        elif value == "no":
+        elif value == value_3:
             return value
         else:
-            value = input("Erreur ! Répétez votre réponse (yes/no): ")
+            value = input("Erreur ! Répétez votre réponse : ")
 def Ask_only_number(message: str):
     while True:
         value: str = input(message)
@@ -29,7 +31,7 @@ def Ask_Min_Max():
             return min, max
         print("Erreur")
 
-def nb_random():
+def get_random():
     (a,b) = Ask_Min_Max()
     nb: int = random.randint(a,b)
     return nb
@@ -48,13 +50,13 @@ def More_or_Less(a: int, b: int, c: int):
 
 def Replay(message: str):
     while message == "yes":
-        nb_AI: int = nb_random()
+        nb_AI: int = get_random()
         essai: int = 10
         while True:
             essai = essai - 1
             nb: int = Ask_only_number("Donner un nombre: ")
             result: str = More_or_Less(nb, nb_AI, essai)
-            print(result,"(il vous reste",str(essai), "essai")
+            print(result,"il vous reste",str(essai), "essai")
 
             if result == "Victoire" or result == "Perdu":
                 message:str =response_replay("Voulez vous rejouer ? (yes/no): ")
@@ -62,27 +64,4 @@ def Replay(message: str):
     print("Merci d'avoir joué !")
 
 
-
-
-
-def Game():
-    nb_AI: int = nb_random()
-    essai:int = 10
-    while True:
-        essai=essai-1
-        nb: int = Ask_only_number("Donner un nombre: ")
-        result:str =More_or_Less(nb,nb_AI,essai)
-        print(result,"il vous reste",str(essai), "essai")
-
-        if result == "Victoire" or result == "Perdu":
-            value:str =response_replay("Voulez vous rejouer ? (yes/no): ")
-            return value
-
-
-
-
-
-
-
-response:str = Game()
-Replay(response)
+Replay("yes")
